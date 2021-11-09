a2525e6283067d0a6965f3eb27fe8e69b1d94c3b
barbarabrito <barbaramabrito@gmail.com>
2021-11-09 17:58:29 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+
+listahaikai = [
+
+	'item vazio',
+	'duas folhas na sandália <br><br>o outono<br><br>também quer andar<br><br><br> <strong>- Paulo Leminski </strong>' ,
+	'casca oca <br><br> a cigarra <br><br> cantou-se toda <br><br><br><strong>- Bashō </strong>',
+	'橋 桁 や 陽 は さ し な が ら ⼣ 霞   北 枝<br><br>hashigeta ya / hi wa sashi nagara / yûgasumi<br><br><br>O sol brilha<br><br>Nas vigas da ponte -<br><br>Névoa da tarde.<br><br><br><strong>- Hokushi</strong>',
+	'蝶鳥の浮つき立つや花の雲<br><br>chō tori no / uwatsuki tatsu ya / hana no kumo<br><br><br>Borboletas e<br><br>aves agitam voo:<br><br>nuvem de flores.<br><br><br> <strong>- Bashō</strong>',
+	'yake ni keri / do mo hana wa / chiri ni keri<br><br><br> Tudo levou o fogo – <br><br> Mas pelo menos as flores <br><br>Já haviam caído.<br><br><br> <strong>- Hokushi</strong>',
+	'harusame ya / utsukushû naru / mono bakari<br><br><br>Chuva de primavera -<br><br>Todas as coisas<br><br>Parecem mais bonitas.<br><br><br><strong>- Chiyo-ni</strong>',
+	'⼭ の ⽉ 花 盗 ⼈ を 照 ら し 給 ふ ⼀ 茶<br><br>yama no tsuki hana nusubito o terashi tamou<br><br><br>A lua da montanha<br><br>Gentilmente ilumina<br><br>O ladrão de flores.<br><br><br><strong>- Kobayashi Issa</strong>',
+	'O tempo<br><br> entre o sopro<br><br>e o apagar da vela<br><br><br><strong>- Paulo Leminski</strong>',
+	'tobi-ume ya / karogaroshiku mo / kami no haru<br><br><br> Voa o papel <br><br>Junto às flores de ameixeira -<br><br>Primavera dos deuses.<br><br><br><strong>- Arakida Moritake</strong>',
+	'kare-eda ni / karasu no tomari keri / aki no kure<br><br><br>Um corvo pousado<br><br>Num ramo seco -<br><br>Entardecer de outono<br><br><br><strong>- Bashō</strong>',
+	'samidare ya / aru yo hisoka ni / matsu no tsuki<br><br><br>Chuvas de verão -<br><br>E certa noite, de mansinho,<br><br>A lua entre os pinheiros<br><br><br><strong>- Ôshima Ryôta</strong>',
+	'Inazuma ni / koboruru oto ya / take no tsuyu<br><br><br>Com a luz do relâmpago,<br><br>Barulhos de pingos -<br><br>orvalho nos bambus.<br><br><br><strong>- Yosa Buson</strong>',
+	'Rosa (que exibida!)<br><br>que posa assim toda prosa<br><br>é a prova de vida.<br><br><br><strong>- Paulo Leminski</strong>',
+	'Ume ga ka ni / notto hi no deru / yamaji kana<br><br><br>No perfume das flores de ameixa,<br><br>O sol de súbito surge –<br><br>Ah, o caminho da montanha<br><br><br><strong>Bashō</strong>',
+	'no ni yama ni / ugoku mono nashi / yuki no asa<br><br><br>No campo, nas montanhas,<br><br>Nada se move: <br><br>Manhã de neve.<br><br><br><strong>- Chiyo-jo</strong>',
+	'Shizukasa ya / ochiba o ariku / tori no oto<br><br><br>Quietude -<br><br>o barulho do pássaro<br><br>pisando as folhas secas.<br><br><br><strong>- Ryúshi</strong>',
+	'shira ume ni / akuru yo bakari to / nari ni ker<br><br><br>Ameixeiras brancas -<br><br>Assim a alva rompe as trevas<br><br>deste dia em diante<br><br><br><strong>- Yosa Buson</strong>',
+	'yane ni neru / nushi nashi neko ya / haru no ame <br><br><br>Um gato sem dono<br><br>Dormindo sobre o telhado -<br><br>Chuva de primavera.<br><br><br><strong>- Taigi</strong>',
+	'Haru no hi ya / niwa ni suzume no / suna abite<br><br><br>Dia de primavera -<br><br>Os pardais no jardim<br><br>Tomam banho de areia.<br><br><br><strong>- Onitsura</strong>',
+	'rôsoku ni / shizumari kaeru / botan kana<br><br><br>Assim como a vela<br><br>Mergulhada no silêncio <br><br>A peônia.<br><br><br><strong>- Kyoroku</strong>',
+	'kozo kotoshi / tsuranuku bô no / gotoki mono<br><br><br>O ano velho e o novo,<br><br>Como se houvesse um cajado<br><br>A trespassá-los<br><br><br><strong>- Kyoshi</strong> ',
+	'tsure mo naku / no ni suterareshi / fuyu no tsuki<br><br><br>Sem ter companhia<br><br>E abandonada no campo<br><br>A lua de inverno.<br><br><br><strong>- Roseki</strong>',
+	'kakashi kara / kakashi e wataru / suzume kana<br><br><br>Os pardais voam<br><br>De espantalho para<br><br>Espantalho.<br><br><br><strong>- Sazanami</strong>',
+	'karakasa no / hitotsu sugiyuku / yuki no kure<br><br><br>Na tarde de neve<br><br>Passa, desaparecendo,<br><br>Um só guarda-chuva.<br><br><br><strong>- Yaha</strong>',
+	'hasu no hana / saku ya sabishiki / teishajô<br><br><br>A solidão<br><br>Da estação de trem -<br><br>Flores de lótus.<br><br><br><strong>- Shiki</strong>',
+	'海くれて鴨のこゑほのかに白し<br><br>umi kurete / kamo no koe honoka ni / shiroshi<br><br><br>Anoitece no mar -<br><br>Os gritos dos patos selvagens,<br><br>Vagamente brancos.<br><br><br><strong>- Bashō</strong>',
+	'tori ichiwa / michizure ni shite / kareno kana<br><br><br>Apenas um pássaro<br><br>Companheiro do caminho<br><br>Pelo campo seco.<br><br><br><strong>- Senna</strong>',
+	'kumo no mine / mizu naki kawa o / watari keri<br><br><br>Atravessando o céu<br><br>Sobre o rio sem água -<br><br>Cúmulos-nimbos.<br><br><br><strong>- Shiki</strong>',
+	'kiete mo sen /  ariake tsuki no /  hama chidori<br><br><br>Logo se esvaecem<br><br>Como a lua na alvorada -<br><br>Batuíras da praia.<br><br><br><strong>- Miura Chora</strong>',	
+	'hi no ko haku /  kisha no yuku te no /  nobi akari<br><br><br>Cuspindo fagulhas<br><br>O trem rumo à claridade<br><br>Da queimada ao longe.<br><br><br><strong>- Rokô Hatayama</strong>',
+	'hana uri no /  kite iru eki no /  chô-chô kana<br><br><br>Parada do trem -<br><br>Com o vendedor de flores<br><br>Vêm as borboletas.<br><br><br><strong>- Sôshi Nakajima</strong>'
+	
+];
+
+
+window.onload = function getaHaiku(){
+
+	datadehoje();
+
+	var hoje = new Date();
+
+	var gh = hoje.getDate();
+
+	var haikai = listahaikai[gh]; 
+	
+	document.getElementById('h').innerHTML = haikai;
+
+};
+
+
+function datadehoje() {
+
+	var today = new Date();
+
+	var year = today.getFullYear();
+
+	var day = today.getDate();
+
+	var diasdasemana = ['Domingo', 'Segunda-Feira','Terça-Feira','Quarta-Feira',
+	'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
+
+	weekday = diasdasemana[today.getDay()];
+
+	var mesPtBr = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio','Junho','Julho', 'Agosto',
+	'Setembro','Outubro','Novembro','Dezembro']; 
+
+	month = mesPtBr[today.getMonth()];
+	
+	document.getElementById('dataatual').innerHTML = (weekday+', '+day+' de '+month+' de '+year);	
+	
+}
+
+
+/*
+var max = 31;
+
+
+var valorAleatorio = Math.floor((Math.random() * max)) + 1;
+
+window.onload = function sorteiaHaiku(){
+
+	datadehoje();
+
+	var haikai = listahaikai[valorAleatorio]; 
+	
+	document.getElementById('h').innerHTML = haikai;
+	
+
+};
+*/
