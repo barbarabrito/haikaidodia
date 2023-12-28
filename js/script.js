f97f0efcf33baecfa6274decf74d1c389587613d
barbarabrito <barbaramabrito@gmail.com>
2023-12-28 18:21:58 -0300

    refactor

--- js/script.js
+++ js/script.js
@@ -1,59 +1,74 @@
+window.onload = function init() {
+  const today = getCurrentDate();
+  const day = today.getDate();
 
-function _0x5c98(_0x40c26e,_0x16d7c2){var _0x15526e=_0x1552();return _0x5c98=function(_0x5c9845,_0x481b00){_0x5c9845=_0x5c9845-0xd5;var _0x10d933=_0x15526e[_0x5c9845];return _0x10d933;},_0x5c98(_0x40c26e,_0x16d7c2);}function _0x1552(){var _0x506182=['7538481bcCsMV','hi\x20no\x20ko\x20haku\x20/\x20\x20kisha\x20no\x20yuku\x20te\x20no\x20/\x20\x20nobi\x20akari<br><br><br>Cuspindo\x20fagulhas<br><br>O\x20trem\x20rumo\x20à\x20claridade<br><br>Da\x20queimada\x20ao\x20longe.<br><br><br><strong>-\x20Rokô\x20Hatayama</strong>','tsure\x20mo\x20naku\x20/\x20no\x20ni\x20suterareshi\x20/\x20fuyu\x20no\x20tsuki<br><br><br>Sem\x20ter\x20companhia<br><br>E\x20abandonada\x20no\x20campo<br><br>A\x20lua\x20de\x20inverno.<br><br><br><strong>-\x20Roseki</strong>','Ume\x20ga\x20ka\x20ni\x20/\x20notto\x20hi\x20no\x20deru\x20/\x20yamaji\x20kana<br><br><br>No\x20perfume\x20das\x20flores\x20de\x20ameixa,<br><br>O\x20sol\x20de\x20súbito\x20surge\x20–<br><br>Ah,\x20o\x20caminho\x20da\x20montanha<br><br><br><strong>Bashō</strong>','hana\x20uri\x20no\x20/\x20\x20kite\x20iru\x20eki\x20no\x20/\x20\x20chô-chô\x20kana<br><br><br>Parada\x20do\x20trem\x20-<br><br>Com\x20o\x20vendedor\x20de\x20flores<br><br>Vêm\x20as\x20borboletas.<br><br><br><strong>-\x20Sôshi\x20Nakajima</strong>','kiete\x20mo\x20sen\x20/\x20\x20ariake\x20tsuki\x20no\x20/\x20\x20hama\x20chidori<br><br><br>Logo\x20se\x20esvaecem<br><br>Como\x20a\x20lua\x20na\x20alvorada\x20-<br><br>Batuíras\x20da\x20praia.<br><br><br><strong>-\x20Miura\x20Chora</strong>','9gzvIWb','Shizukasa\x20ya\x20/\x20ochiba\x20o\x20ariku\x20/\x20tori\x20no\x20oto<br><br><br>Quietude\x20-<br><br>o\x20barulho\x20do\x20pássaro<br><br>pisando\x20as\x20folhas\x20secas.<br><br><br><strong>-\x20Ryúshi</strong>','Haru\x20no\x20hi\x20ya\x20/\x20niwa\x20ni\x20suzume\x20no\x20/\x20suna\x20abite<br><br><br>Dia\x20de\x20primavera\x20-<br><br>Os\x20pardais\x20no\x20jardim<br><br>Tomam\x20banho\x20de\x20areia.<br><br><br><strong>-\x20Onitsura</strong>','5017650rYosyn','3133964KIHMRr','72uGHhdd','5YgQuje','yane\x20ni\x20neru\x20/\x20nushi\x20nashi\x20neko\x20ya\x20/\x20haru\x20no\x20ame\x20<br><br><br>Um\x20gato\x20sem\x20dono<br><br>Dormindo\x20sobre\x20o\x20telhado\x20-<br><br>Chuva\x20de\x20primavera.<br><br><br><strong>-\x20Taigi</strong>','kumo\x20no\x20mine\x20/\x20mizu\x20naki\x20kawa\x20o\x20/\x20watari\x20keri<br><br><br>Atravessando\x20o\x20céu<br><br>Sobre\x20o\x20rio\x20sem\x20água\x20-<br><br>Cúmulos-nimbos.<br><br><br><strong>-\x20Shiki</strong>','⼭\x20の\x20⽉\x20花\x20盗\x20⼈\x20を\x20照\x20ら\x20し\x20給\x20ふ\x20⼀\x20茶<br><br>yama\x20no\x20tsuki\x20hana\x20nusubito\x20o\x20terashi\x20tamou<br><br><br>A\x20lua\x20da\x20montanha<br><br>Gentilmente\x20ilumina<br><br>O\x20ladrão\x20de\x20flores.<br><br><br><strong>-\x20Kobayashi\x20Issa</strong>','kakashi\x20kara\x20/\x20kakashi\x20e\x20wataru\x20/\x20suzume\x20kana<br><br><br>Os\x20pardais\x20voam<br><br>De\x20espantalho\x20para<br><br>Espantalho.<br><br><br><strong>-\x20Sazanami</strong>','11262176sZcuqv','橋\x20桁\x20や\x20陽\x20は\x20さ\x20し\x20な\x20が\x20ら\x20⼣\x20霞\x20\x20\x20北\x20枝<br><br>hashigeta\x20ya\x20/\x20hi\x20wa\x20sashi\x20nagara\x20/\x20yûgasumi<br><br><br>O\x20sol\x20brilha<br><br>Nas\x20vigas\x20da\x20ponte\x20-<br><br>Névoa\x20da\x20tarde.<br><br><br><strong>-\x20Hokushi</strong>','casca\x20oca\x20<br><br>\x20a\x20cigarra\x20<br><br>\x20cantou-se\x20toda\x20<br><br><br><strong>-\x20Bashō\x20</strong>','hasu\x20no\x20hana\x20/\x20saku\x20ya\x20sabishiki\x20/\x20teishajô<br><br><br>A\x20solidão<br><br>Da\x20estação\x20de\x20trem\x20-<br><br>Flores\x20de\x20lótus.<br><br><br><strong>-\x20Shiki</strong>','11EKbHwT','yake\x20ni\x20keri\x20/\x20do\x20mo\x20hana\x20wa\x20/\x20chiri\x20ni\x20keri<br><br><br>\x20Tudo\x20levou\x20o\x20fogo\x20–\x20<br><br>\x20Mas\x20pelo\x20menos\x20as\x20flores\x20<br><br>Já\x20haviam\x20caído.<br><br><br>\x20<strong>-\x20Hokushi</strong>','O\x20tempo<br><br>\x20entre\x20o\x20sopro<br><br>e\x20o\x20apagar\x20da\x20vela<br><br><br><strong>-\x20Paulo\x20Leminski</strong>','shira\x20ume\x20ni\x20/\x20akuru\x20yo\x20bakari\x20to\x20/\x20nari\x20ni\x20ker<br><br><br>Ameixeiras\x20brancas\x20-<br><br>Assim\x20a\x20alva\x20rompe\x20as\x20trevas<br><br>deste\x20dia\x20em\x20diante<br><br><br><strong>-\x20Yosa\x20Buson</strong>','rôsoku\x20ni\x20/\x20shizumari\x20kaeru\x20/\x20botan\x20kana<br><br><br>Assim\x20como\x20a\x20vela<br><br>Mergulhada\x20no\x20silêncio\x20<br><br>A\x20peônia.<br><br><br><strong>-\x20Kyoroku</strong>','karakasa\x20no\x20/\x20hitotsu\x20sugiyuku\x20/\x20yuki\x20no\x20kure<br><br><br>Na\x20tarde\x20de\x20neve<br><br>Passa,\x20desaparecendo,<br><br>Um\x20só\x20guarda-chuva.<br><br><br><strong>-\x20Yaha</strong>','149163LXbUGo','samidare\x20ya\x20/\x20aru\x20yo\x20hisoka\x20ni\x20/\x20matsu\x20no\x20tsuki<br><br><br>Chuvas\x20de\x20verão\x20-<br><br>E\x20certa\x20noite,\x20de\x20mansinho,<br><br>A\x20lua\x20entre\x20os\x20pinheiros<br><br><br><strong>-\x20Ôshima\x20Ryôta</strong>','no\x20ni\x20yama\x20ni\x20/\x20ugoku\x20mono\x20nashi\x20/\x20yuki\x20no\x20asa<br><br><br>No\x20campo,\x20nas\x20montanhas,<br><br>Nada\x20se\x20move:\x20<br><br>Manhã\x20de\x20neve.<br><br><br><strong>-\x20Chiyo-jo</strong>','375759oNLaaJ','Rosa\x20(que\x20exibida!)<br><br>que\x20posa\x20assim\x20toda\x20prosa<br><br>é\x20a\x20prova\x20de\x20vida.<br><br><br><strong>-\x20Paulo\x20Leminski</strong>','kozo\x20kotoshi\x20/\x20tsuranuku\x20bô\x20no\x20/\x20gotoki\x20mono<br><br><br>O\x20ano\x20velho\x20e\x20o\x20novo,<br><br>Como\x20se\x20houvesse\x20um\x20cajado<br><br>A\x20trespassá-los<br><br><br><strong>-\x20Kyoshi</strong>\x20','449708iNraQl','蝶鳥の浮つき立つや花の雲<br><br>chō\x20tori\x20no\x20/\x20uwatsuki\x20tatsu\x20ya\x20/\x20hana\x20no\x20kumo<br><br><br>Borboletas\x20e<br><br>aves\x20agitam\x20voo:<br><br>nuvem\x20de\x20flores.<br><br><br>\x20<strong>-\x20Bashō</strong>','tobi-ume\x20ya\x20/\x20karogaroshiku\x20mo\x20/\x20kami\x20no\x20haru<br><br><br>\x20Voa\x20o\x20papel\x20<br><br>Junto\x20às\x20flores\x20de\x20ameixeira\x20-<br><br>Primavera\x20dos\x20deuses.<br><br><br><strong>-\x20Arakida\x20Moritake</strong>','item\x20vazio','harusame\x20ya\x20/\x20utsukushû\x20naru\x20/\x20mono\x20bakari<br><br><br>Chuva\x20de\x20primavera\x20-<br><br>Todas\x20as\x20coisas<br><br>Parecem\x20mais\x20bonitas.<br><br><br><strong>-\x20Chiyo-ni</strong>'];_0x1552=function(){return _0x506182;};return _0x1552();}var _0x3db1fa=_0x5c98;(function(_0x359eaa,_0xc68d1b){var _0x236272=_0x5c98,_0x4500b5=_0x359eaa();while(!![]){try{var _0x1f8e40=parseInt(_0x236272(0xdb))/0x1+-parseInt(_0x236272(0xde))/0x2*(parseInt(_0x236272(0xe9))/0x3)+parseInt(_0x236272(0xed))/0x4*(-parseInt(_0x236272(0xef))/0x5)+-parseInt(_0x236272(0xee))/0x6*(-parseInt(_0x236272(0xd8))/0x7)+parseInt(_0x236272(0xf4))/0x8+parseInt(_0x236272(0xe3))/0x9+parseInt(_0x236272(0xec))/0xa*(-parseInt(_0x236272(0xf8))/0xb);if(_0x1f8e40===_0xc68d1b)break;else _0x4500b5['push'](_0x4500b5['shift']());}catch(_0x1c2562){_0x4500b5['push'](_0x4500b5['shift']());}}}(_0x1552,0xdfe26),listahaikai=[_0x3db1fa(0xe1),'duas\x20folhas\x20na\x20sandália\x20<br><br>o\x20outono<br><br>também\x20quer\x20andar<br><br><br>\x20<strong>-\x20Paulo\x20Leminski\x20</strong>',_0x3db1fa(0xf6),_0x3db1fa(0xf5),_0x3db1fa(0xdf),_0x3db1fa(0xf9),_0x3db1fa(0xe2),_0x3db1fa(0xf2),_0x3db1fa(0xfa),_0x3db1fa(0xe0),'kare-eda\x20ni\x20/\x20karasu\x20no\x20tomari\x20keri\x20/\x20aki\x20no\x20kure<br><br><br>Um\x20corvo\x20pousado<br><br>Num\x20ramo\x20seco\x20-<br><br>Entardecer\x20de\x20outono<br><br><br><strong>-\x20Bashō</strong>',_0x3db1fa(0xd9),'Inazuma\x20ni\x20/\x20koboruru\x20oto\x20ya\x20/\x20take\x20no\x20tsuyu<br><br><br>Com\x20a\x20luz\x20do\x20relâmpago,<br><br>Barulhos\x20de\x20pingos\x20-<br><br>orvalho\x20nos\x20bambus.<br><br><br><strong>-\x20Yosa\x20Buson</strong>',_0x3db1fa(0xdc),_0x3db1fa(0xe6),_0x3db1fa(0xda),_0x3db1fa(0xea),_0x3db1fa(0xd5),_0x3db1fa(0xf0),_0x3db1fa(0xeb),_0x3db1fa(0xd6),_0x3db1fa(0xdd),_0x3db1fa(0xe5),_0x3db1fa(0xf3),_0x3db1fa(0xd7),_0x3db1fa(0xf7),'海くれて鴨のこゑほのかに白し<br><br>umi\x20kurete\x20/\x20kamo\x20no\x20koe\x20honoka\x20ni\x20/\x20shiroshi<br><br><br>Anoitece\x20no\x20mar\x20-<br><br>Os\x20gritos\x20dos\x20patos\x20selvagens,<br><br>Vagamente\x20brancos.<br><br><br><strong>-\x20Bashō</strong>','tori\x20ichiwa\x20/\x20michizure\x20ni\x20shite\x20/\x20kareno\x20kana<br><br><br>Apenas\x20um\x20pássaro<br><br>Companheiro\x20do\x20caminho<br><br>Pelo\x20campo\x20seco.<br><br><br><strong>-\x20Senna</strong>',_0x3db1fa(0xf1),_0x3db1fa(0xe8),_0x3db1fa(0xe4),_0x3db1fa(0xe7)]);
+  getHaiku(day)
+    .then(haiku => {
+      document.getElementById('haiku').innerHTML = haiku;
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
-window.onload = function getaHaiku(){
-
-	datadehoje();
-
-	var hoje = new Date();
-
-	var gh = hoje.getDate();
-
-	var haikai = listahaikai[gh]; 
-	
-	document.getElementById('h').innerHTML = haikai;
-
-
+  displayCurrentDate(today);
 };
 
-
-function datadehoje() {
-
-	var today = new Date();
-
-	var year = today.getFullYear();
-
-	var day = today.getDate();
-
-	var diasdasemana = ['Domingo', 'Segunda-Feira','Terça-Feira','Quarta-Feira',
-	'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
-
-	weekday = diasdasemana[today.getDay()];
-
-	var mesPtBr = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio','Junho','Julho', 'Agosto',
-	'Setembro','Outubro','Novembro','Dezembro']; 
-
-	month = mesPtBr[today.getMonth()];
-	
-	document.getElementById('dataatual').innerHTML = (weekday+', '+day+' de '+month+' de '+year);	
-	
+function getCurrentDate() {
+  const today = new Date();
+  return today;
 }
 
+function getHaiku(day) {
+  return new Promise((resolve, reject) => {
+    fetch('haikus/haikus.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const haiku = data.haikus[day];
+        resolve(haiku);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
 
-/*
-var max = 31;
+function displayCurrentDate(today) {
+  const year = today.getFullYear();
+  const day = today.getDate();
+  const weekday = weekDays[today.getDay()];
+  const month = months[today.getMonth()];
 
+  document.getElementById('current_date_time').innerHTML = (
+    weekday + ', ' + day + ' de ' + month + ' de ' + year
+  );
+}
 
-var valorAleatorio = Math.floor((Math.random() * max)) + 1;
+const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro'
+];
 
-window.onload = function sorteiaHaiku(){
-
-	datadehoje();
-
-	var haikai = listahaikai[valorAleatorio]; 
-	
-	document.getElementById('h').innerHTML = haikai;
-	
-
-};
-*/
+const weekDays = [
+  'Domingo',
+  'Segunda-Feira',
+  'Terça-Feira',
+  'Quarta-Feira',
+  'Quinta-Feira',
+  'Sexta-Feira',
+  'Sábado'
+];
